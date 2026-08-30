ac14cabf4f46060d0e244bf0e16ae392bad716d4
WillDone-MT <wildanmufti350@gmail.com>
2026-08-30 20:15:25 +0700

    Make file be executed before build into pdfs

--- scripts/build_local.ps1
+++ scripts/build_local.ps1
@@ -5,5 +5,5 @@ Write-Host "Activating python .venv..." -ForegroundColor Cyan
 (Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& .venv\Scripts\Activate.ps1)
 
 python scripts/inject_page_exports.py
-jupyter-book build --pdf
+jupyter-book build --execute --pdf
 jupyter-book start
