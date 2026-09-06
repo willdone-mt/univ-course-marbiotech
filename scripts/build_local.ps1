012b2b4c5eaaf130675e9db1335814dd1b754db2
WillDone-MT <wildanmufti350@gmail.com>
2026-09-06 18:13:04 +0700

    fix file name in `build_local`

--- scripts/build_local.ps1
+++ scripts/build_local.ps1
@@ -4,7 +4,7 @@
 Write-Host "Activating python .venv..." -ForegroundColor Cyan
 (Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& .venv\Scripts\Activate.ps1)
 
-python scripts/patch-mystmd-typst.py
+python scripts/patch_jupyterbook_typst.py
 python scripts/inject_page_exports.py
 python scripts/xlsx_to_markdown.py
 python scripts/render_mermaid.py
